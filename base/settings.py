4f5f0789bc15f5c31f9b35a05c4fcd2693de4db7
hangoh <74915663+hangoh@users.noreply.github.com>
2022-03-16 22:00:27 +0900

    Update settings.py

--- base/settings.py
+++ base/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ["django-public-chat.herokuapp.com","127.0.0.1"]
